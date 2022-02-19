66a3299d1a14c18a4ffde676df376f83ef2f2776
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2022-02-19 16:23:55 +0800

    Update del_fragmented_files.sh

--- del_fragmented_files.sh
+++ del_fragmented_files.sh
@@ -6,5 +6,6 @@ rsync --delete-before -a -H block/ target_del/
 #没啥用,这里时删除,同步属性#-a, --archive               archive mode; equals -rlptgoD (no -H,-A,-X)
 #没啥用,这里时删除,同步硬链接#-H, --hard-links            preserve hard links
 
+# 打算在文件夹内删除时，避免ls 过多文件才进行删除，可以提速
 #ls | xargs -n 10 rm -fr {}
 # -n 每组10个文件名传给 rm -fr {}
