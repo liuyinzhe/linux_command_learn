6c0081e62e4c232e71b26ee8a5ce610c7aede7a5
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2022-02-19 16:22:54 +0800

    Create rsync_del_fragmented_files.sh

---
+++ rsync_del_fragmented_files.sh
@@ -0,0 +1,10 @@
+mkdir block
+rsync --delete-before -a -H block/ target_del/
+#block 空文件
+#target_del 要删除全部内容的文件夹
+
+#没啥用,这里时删除,同步属性#-a, --archive               archive mode; equals -rlptgoD (no -H,-A,-X)
+#没啥用,这里时删除,同步硬链接#-H, --hard-links            preserve hard links
+
+#ls | xargs -n 10 rm -fr {}
+# -n 每组10个文件名传给 rm -fr {}
