3ca51b17a665b8675d67d68e4a01af6bdff51d79
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2023-05-02 16:46:15 +0800

    Update command.bash

--- command.bash
+++ command.bash
@@ -7,3 +7,12 @@ tree -f -i
 
 # 查看tsv 文件,宽度保持一致
 column -t  input.tsv
+<< COMMENT
+-s sep	指定一组字符，用于分隔-t选项的列
+-t	确定输入包含的列数并创建表
+-n	禁用将多个相邻分隔符合并为单个分隔符行为
+-c columns	输出格式化为显示columns列宽
+-x	在填充行之前填充列
+-e	不要忽略空白行
+
+COMMENT
