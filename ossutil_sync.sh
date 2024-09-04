c46e249b52c72f4717023317229718406e31a74b
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-09-04 12:46:23 +0800

    Update ossutil_sync.sh

--- ossutil_sync.sh
+++ ossutil_sync.sh
@@ -3,3 +3,12 @@
   -f \
   oss://xxxxxxxxxxx/data/test \
   /data/rawdata/  >log 2>err
+
+
+# .ossutilconfig
+
+#[Credentials]
+#language=CH
+#accessKeyID=xxxxxxxxxxxxxxxxxxxxxxx
+#accessKeySecret=xxxxxxxxxxxx
+#endpoint=oss-cn-shanghai.aliyuncs.com #oss-cn-beijing.aliyuncs.com
